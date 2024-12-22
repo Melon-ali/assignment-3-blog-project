28f5394cb988ab1b660655108a6aa55a0c3a6aae
Melon-ali <86640181+Melon-ali@users.noreply.github.com>
2024-12-22 08:57:40 +0600

    user section updateed

--- src/app/modules/users/user.routes.ts
+++ src/app/modules/users/user.routes.ts
@@ -1,14 +1,7 @@
 import express from 'express';
-import { UserController } from './user.controller';
-import { UserValidtions } from './user.validation';
-import validateRequest from '../../middlwares/validateRequest';
+
 
 const router = express.Router();
-//create user
-router.post(
-  '/register',
-  validateRequest(UserValidtions.userchmeavalidations),
-  UserController.createUser
-);
 
 export const UserRoutes = router;
+ 
